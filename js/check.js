ea6c6c6f47e006b9348d53c32f787aa521990dd5
Karansingh Chauhan <31315654+c2karansingh@users.noreply.github.com>
2018-06-12 17:44:43 +0530

    added method for extended permissions

---
+++ js/check.js
@@ -0,0 +1,125 @@
+
+window.fbAsyncInit = function() 
+{
+
+    FB.init
+    (
+    	{
+      		appId      : 1856495984403203,
+      		cookie     : true,
+      		xfbml      : true,
+      		version    : 'v3.0'
+    	}
+    );
+    
+
+  FB.AppEvents.logPageView();   
+    
+	FB.getLoginStatus
+	(
+		function(response) 
+		{
+
+      if(response.authResponse)
+      {
+        
+        FB.api
+        (
+        
+          '/'+response.authResponse.userID+"/permissions"
+        
+          , 
+          
+          'get'
+        
+          ,
+
+          
+          // {
+          //   fields:"id,name,email"
+          // }
+          
+          // ,
+
+          function(response) 
+          {
+                  // document.querySelector("#heading").innerHTML = 'welcome to the app, <strong>' + response.name +'<strong> !';
+                  // document.querySelector("#mail").innerHTML = 'your E-mail id is, <strong>' + response.email +'<strong> !';
+                  // console.log(response.email);
+                  console.log(response);
+                  
+          }
+
+        );
+
+        FB.api
+        (
+          '/me'
+          
+          ,
+
+          
+          {
+            fields:"id,name,email,gender"
+          }
+          
+          ,
+
+          function(response) 
+          {
+                  console.log(response);
+                  if(response.gender==undefined)
+                  {
+
+                    FB.login
+                    (
+                      function(response) 
+                      {
+                        alert("Done!");
+                      }
+                      
+                      ,
+                      
+                      {
+                        scope: "user_gender"
+                      }
+                    );
+                  
+                  }  
+                  document.querySelector("#heading").innerHTML = 'welcome to the app, <strong>' + response.name +'<strong> !';
+                  document.querySelector("#mail").innerHTML = 'your E-mail id is, <strong>' + response.email +'<strong> !';
+                  console.log(response.gender);
+
+          }
+        );
+        
+      
+      }  
+      else
+            window.location.href="http://localhost:8080/facebookFetch/index.html";     
+		  
+    }
+	);
+};
+
+
+
+
+
+
+
+
+
+(
+	function(d, s, id)
+	{
+    	var js, fjs = d.getElementsByTagName(s)[0];
+     	if (d.getElementById(id)) {return;}
+    	 js = d.createElement(s); js.id = id;
+     	js.src = "https://connect.facebook.net/en_US/sdk.js";
+     	fjs.parentNode.insertBefore(js, fjs);
+    }
+
+    (document, 'script', 'facebook-jssdk')
+
+);
